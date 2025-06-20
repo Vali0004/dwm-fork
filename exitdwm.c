d40f84dbc0cdefc3a15426d5150cba4882756201
Vali0004 <diorcheats.vali@gmail.com>
2025-06-19 20:39:38 -0400

    Update exitmenu

--- exitdwm.c
+++ exitdwm.c
@@ -6,13 +6,13 @@ void exitdwm ()
 # if							   \
 	defined S_LOCK				|| \
 	defined S_RESTART_DWM		|| \
-	defined S_OFFSCREEN			|| \
+	defined S_SLEEP			|| \
 	defined S_EXIT				|| \
 	defined S_REBOOT			|| \
 	defined S_SHUTDOWN			|| \
 	defined S_LOCK_ICON			|| \
 	defined S_RESTART_DWM_ICON	|| \
-	defined S_OFFSCREEN_ICON	|| \
+	defined S_SLEEP_ICON	|| \
 	defined S_EXIT_ICON			|| \
 	defined S_REBOOT_ICON		|| \
 	defined S_SHUTDOWN_ICON		|| \
@@ -22,14 +22,14 @@ void exitdwm ()
 # endif
 
 # define S_LOCK "Lock"
-# define S_RESTART_DWM "restart Dwm"
-# define S_OFFSCREEN "Off-screen"
+# define S_RESTART_DWM "Restart dwm"
+# define S_SLEEP "Sleep"
 # define S_EXIT "Exit"
 # define S_REBOOT "Reboot"
 # define S_SHUTDOWN "Shutdown"
 # define S_LOCK_ICON "\uf023"			// <= FontAwesome icons
 # define S_RESTART_DWM_ICON "\uf01e"
-# define S_OFFSCREEN_ICON "\uf108"
+# define S_SLEEP_ICON "\uf108"
 # define S_EXIT_ICON "\uf2f5"
 # define S_REBOOT_ICON "\uf021"
 # define S_SHUTDOWN_ICON "\uf011"
@@ -62,7 +62,7 @@ void exitdwm ()
 
 	if (strcmp (exit_action, S_LOCK) == 0) system ("slock & sleep .5; xset dpms force off");
 	else if (strcmp (exit_action, S_RESTART_DWM) == 0) quit (& (const Arg) {1});
-	else if (strcmp (exit_action, S_OFFSCREEN) == 0) system ("sleep .5; xset dpms force off");
+	else if (strcmp (exit_action, S_SLEEP) == 0) system ("sleep .5; xset dpms force off");
 	else if (strcmp (exit_action, S_EXIT) == 0) quit (& (const Arg) {0});
 	else if (strcmp (exit_action, S_REBOOT) == 0) system ("systemctl reboot");
 	else if (strcmp (exit_action, S_SHUTDOWN) == 0) system ("systemctl poweroff -i");
@@ -72,13 +72,13 @@ close_streams:
 
 # undef S_LOCK
 # undef S_RESTART_DWM
-# undef S_OFFSCREEN
+# undef S_SLEEP
 # undef S_EXIT
 # undef S_REBOOT
 # undef S_SHUTDOWN
 # undef S_LOCK_ICON
 # undef S_RESTART_DWM_ICON
-# undef S_OFFSCREEN_ICON
+# undef S_SLEEP_ICON
 # undef S_EXIT_ICON
 # undef S_REBOOT_ICON
 # undef S_SHUTDOWN_ICON
