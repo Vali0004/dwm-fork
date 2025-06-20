4b27b95a8a58869a169c28fc5edfaf5d1adbea29
Vali0004 <diorcheats.vali@gmail.com>
2025-06-19 21:50:31 -0400

    Update config

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const unsigned int borderpx  = 1;       		  /* border pixel of windows */
+static const unsigned int snap      = 32;      		  /* snap pixel */
+static const unsigned int gappih    = 20;      		  /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;      		  /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;      		  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;      		  /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;       		  /* 1 means no outer gap when there is only one window */
+static const int showbar            = 1;       		  /* 0 means no bar */
+static const int topbar             = 1;       		  /* 0 means bottom bar */
+static const int usealtbar          = 1;       		  /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; 	  /* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -146,7 +146,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
+	{ MODKEY|ShiftMask, 			XK_r,      quit,          {1} },
 };
 
 /* button definitions */
