6d8386defa70b09d1c882d87dc1ec3cc151d8000
Vali0004 <diorcheats.vali@gmail.com>
2025-07-07 00:31:28 -0400

    Partial rollback

--- config.h
+++ config.h
@@ -1,28 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx    = 1;               /* border pixel of windows */
-static const unsigned int snap        = 32;              /* snap pixel */
-static const unsigned int gappih      = 20;              /* horiz inner gap between windows */
-static const unsigned int gappiv      = 10;              /* vert inner gap between windows */
-static const unsigned int gappoh      = 10;              /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov      = 30;              /* vert outer gap between windows and screen edge */
-static const int smartgaps            = 1;               /* 1 means no outer gap when there is only one window */
-static const int showbar              = 1;               /* 0 means no bar */
-static const int topbar               = 1;               /* 0 means bottom bar */
-static const int usealtbar            = 1;               /* 1 means use non-dwm status bar */
-static const char *altbarclass        = "Polybar";       /* Alternate bar class name */
-static const char *altbarcmd          = "";              /* Alternate bar launch command */
-static const char *fonts[]            = { "monospace:size=10" };
-static const char dmenufont[]         = "monospace:size=10";
-static const char col_gray1[]         = "#0F0F0F";
-static const char col_gray2[]         = "#3B3B3B";
-static const char col_gray3[]         = "#bbbbbb";
-static const char col_gray4[]         = "#eeeeee";
-static const char col_cyan[]          = "#005577";
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
+static const unsigned int borderpx       = 1;               /* border pixel of windows */
+static const unsigned int snap           = 32;              /* snap pixel */
+static const unsigned int gappih         = 10;              /* horiz inner gap between windows */
+static const unsigned int gappiv         = 10;              /* vert inner gap between windows */
+static const unsigned int gappoh         = 10;              /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 20;              /* vert outer gap between windows and screen edge */
+static const unsigned int systraypinning = 0;               /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;               /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;               /* systray spacing */
+static const int systraypinningfailfirst = 1;               /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 0;               /* 0 means no systray */
+static const int smartgaps               = 1;               /* 1 means no outer gap when there is only one window */
+static const int showbar                 = 1;               /* 0 means no bar */
+static const int topbar                  = 1;               /* 0 means bottom bar */
+static const int usealtbar               = 0;               /* 1 means use non-dwm status bar */
+static const char *altbarclass           = "Polybar";       /* Alternate bar class name */
+static const char *altbarcmd             = "";              /* Alternate bar launch command */
+static const char *fonts[]               = { "monospace:size=10" };
+static const char dmenufont[]            = "monospace:size=10";
+static const char col_gray1[]            = "#0F0F0F";
+static const char col_gray2[]            = "#3B3B3B";
+static const char col_gray3[]            = "#bbbbbb";
+static const char col_gray4[]            = "#eeeeee";
+static const char col_cyan[]             = "#005577";
+static const unsigned int baralpha       = 0xd0;
+static const unsigned int borderalpha    = OPAQUE;
+static const char *colors[][3]             = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_gray1, col_gray1  },
